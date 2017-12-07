146b0f17cbb5facbaae8d11e5a7d27abf50f6a2e
Cygni94 <ccoelho.me@gmail.com>
2017-12-06 23:51:27 -0200

    initial commit

---
+++ src/gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin'),
+    browserSync = require('browser-sync').create(),
+    jshint = require('gulp-jshint'),
+    jshintStylish = require('jshint-stylish'),
+    csslint = require('gulp-csslint'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass');
+
+gulp.task('default', ['copy'], function () {
+    gulp.start('build-img', 'usemin');
+});
+
+gulp.task('copy', ['clean'], function () {
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build-img', function () {
+
+    return gulp.src('dist/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function () {
+    return gulp.src('dist/**/*.html')
+        .pipe(usemin({
+            js: [uglify],
+            css: [autoprefixer]
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    });
+
+    gulp.watch('src/**/*').on('change', browserSync.reload);
+
+    gulp.watch('src/js/**/*.js').on('change', function (event) {
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish));
+    });
+
+    gulp.watch('src/css/**/*.css').on('change', function (event) {
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(csslint())
+            .pipe(csslint.reporter());
+    });
+
+    gulp.task('sass', function () {
+        return gulp.src('./sass/**/*.scss')
+            .pipe(sass.sync().on('error', sass.logError))
+            .pipe(gulp.dest('dist/css'));
+    });
+
+    gulp.task('sass:watch', function () {
+        gulp.watch('./sass/**/*.scss', ['sass']);
+    });
+});
