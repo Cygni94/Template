4214e7e511cb2665a1b3ecfae981d9954727dc4a
Cygni94 <ccoelho.me@gmail.com>
2018-01-09 21:01:45 -0200

    a

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,9 @@ let gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css'),
-    sourcemaps = require('gulp-sourcemaps');
-
+    sourcemaps = require('gulp-sourcemaps'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer');
 
 
 // Static Server + watching scss/html files
@@ -27,5 +28,32 @@ gulp.task('sass', function () {
     return gulp.src("src/assets/sass/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("src/assets/css"))
+        .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(cleanCSS({
+            level: {
+                2: {
+                    mergeAdjacentRules: true, // controls adjacent rules merging; defaults to true
+                    mergeIntoShorthands: true, // controls merging properties into shorthands; defaults to true
+                    mergeMedia: true, // controls `@media` merging; defaults to true
+                    mergeNonAdjacentRules: true, // controls non-adjacent rule merging; defaults to true
+                    mergeSemantically: false, // controls semantic merging; defaults to false
+                    overrideProperties: true, // controls property overriding based on understandability; defaults to true
+                    removeEmpty: true, // controls removing empty rules and nested blocks; defaults to `true`
+                    reduceNonAdjacentRules: true, // controls non-adjacent rule reducing; defaults to true
+                    removeDuplicateFontRules: true, // controls duplicate `@font-face` removing; defaults to true
+                    removeDuplicateMediaBlocks: true, // controls duplicate `@media` removing; defaults to true
+                    removeDuplicateRules: true, // controls duplicate rules removing; defaults to true
+                    removeUnusedAtRules: true, // controls unused at rule removing; defaults to false (available since 4.1.0)
+                    restructureRules: true, // controls rule restructuring; defaults to false
+                    skipProperties: [] // controls which properties won't be optimized, defaults to `[]` which means all will be optimized (since 4.1.0)
+                }
+            }
+        }, {
+            compatibility: 'ie8'
+        }))
+        .pipe(sourcemaps.write('sourcemaps'))
+        .pipe(gulp.dest("src/assets/css"))
         .pipe(browserSync.stream());
+
 });
