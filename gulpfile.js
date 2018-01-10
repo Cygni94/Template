cdf6eda7b7337c7ed8e6865427d1c0e6fe80b714
Cygni94 <ccoelho.me@gmail.com>
2018-01-09 22:18:37 -0200

    template atualizado

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,66 @@
 
 // INSTALE O GULP-SPRITESMITH: https://github.com/twolfson/gulp.spritesmith
 
-let gulp = require('gulp'),
+var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
+    htmlmin = require('gulp-htmlmin'),
+    rev = require('gulp-rev'),
+    imagemin = require('gulp-image'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
     cleanCSS = require('gulp-clean-css'),
     sourcemaps = require('gulp-sourcemaps'),
     postcss = require('gulp-postcss'),
+    uncss = require('postcss-uncss'),
+    del = require('del'),
     autoprefixer = require('autoprefixer');
+
+
+// build dist
+
+
+gulp.task('default', ['copy'], function () {
+    gulp.start('build-img', 'usemin');
+});
+
+gulp.task('copy', ['clean'], function () {
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+    return del('dist');
+});
+
+gulp.task('build-img', function () {
+    return gulp.src('dist/assets/img/**/*')
+        .pipe(imagemin({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            guetzli: false,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 100
+        }))
+        .pipe(gulp.dest('dist/assets/img'));
+});
+
+gulp.task('usemin', function () {
+    return gulp.src('./*.html')
+        .pipe(usemin({
+            html: [htmlmin({
+                collapseWhitespace: true
+            })],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [ cleanCSS(), 'concat' ]
+        }))
+        .pipe(gulp.dest('dist/'));
+});
 
 
 // Static Server + watching scss/html files
@@ -25,10 +78,16 @@ gulp.task('server', ['sass'], function () {
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
+    var plugins = [
+        uncss({
+            html: ['src/**/*.html']
+        }),
+    ];
     return gulp.src("src/assets/sass/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("src/assets/css"))
         .pipe(sourcemaps.init())
+        .pipe(postcss(plugins))
         .pipe(postcss([autoprefixer()]))
         .pipe(cleanCSS({
             level: {
