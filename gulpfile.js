47434a24591b831f91b3b05150a71c4f49da0d0d
Cygni94 <ccoelho.me@gmail.com>
2018-02-01 21:12:33 -0200

    ok

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,8 @@ var gulp = require('gulp'),
 
 
 // build dist
-
-
 gulp.task('default', ['copy'], function () {
-    gulp.start('build-img', 'usemin');
+    gulp.start('build-img'); // adaptar usemin e adicionar ", 'usemin'"
 });
 
 gulp.task('copy', ['clean'], function () {
@@ -50,7 +48,7 @@ gulp.task('build-img', function () {
         }))
         .pipe(gulp.dest('dist/assets/img'));
 });
-
+/*
 gulp.task('usemin', function () {
     return gulp.src('./*.html')
         .pipe(usemin({
@@ -63,7 +61,7 @@ gulp.task('usemin', function () {
         }))
         .pipe(gulp.dest('dist/'));
 });
-
+*/
 
 // Static Server + watching scss/html files
 gulp.task('server', ['sass'], function () {
@@ -78,41 +76,13 @@ gulp.task('server', ['sass'], function () {
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
-    var plugins = [
-        uncss({
-            html: ['src/**/*.html']
-        }),
-    ];
     return gulp.src("src/assets/sass/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("src/assets/css"))
         .pipe(sourcemaps.init())
-        .pipe(postcss(plugins))
         .pipe(postcss([autoprefixer()]))
-        .pipe(cleanCSS({
-            level: {
-                2: {
-                    mergeAdjacentRules: true, // controls adjacent rules merging; defaults to true
-                    mergeIntoShorthands: true, // controls merging properties into shorthands; defaults to true
-                    mergeMedia: true, // controls `@media` merging; defaults to true
-                    mergeNonAdjacentRules: true, // controls non-adjacent rule merging; defaults to true
-                    mergeSemantically: false, // controls semantic merging; defaults to false
-                    overrideProperties: true, // controls property overriding based on understandability; defaults to true
-                    removeEmpty: true, // controls removing empty rules and nested blocks; defaults to `true`
-                    reduceNonAdjacentRules: true, // controls non-adjacent rule reducing; defaults to true
-                    removeDuplicateFontRules: true, // controls duplicate `@font-face` removing; defaults to true
-                    removeDuplicateMediaBlocks: true, // controls duplicate `@media` removing; defaults to true
-                    removeDuplicateRules: true, // controls duplicate rules removing; defaults to true
-                    removeUnusedAtRules: true, // controls unused at rule removing; defaults to false (available since 4.1.0)
-                    restructureRules: true, // controls rule restructuring; defaults to false
-                    skipProperties: [] // controls which properties won't be optimized, defaults to `[]` which means all will be optimized (since 4.1.0)
-                }
-            }
-        }, {
-            compatibility: 'ie8'
-        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(sourcemaps.write('sourcemaps'))
         .pipe(gulp.dest("src/assets/css"))
         .pipe(browserSync.stream());
-
 });
